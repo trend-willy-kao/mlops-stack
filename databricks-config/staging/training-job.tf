e618c5ddd34868fbae9034ca499cad4b2d3a1d15
willy-kao <willy_kao@trendmicro.com>
2023-04-14 13:47:39 +0800

    use default runner and test terraform cicd

--- databricks-config/staging/training-job.tf
+++ databricks-config/staging/training-job.tf
@@ -21,7 +21,7 @@ resource "databricks_job" "model_training_job" {
     }
 
     new_cluster {
-      num_workers   = 3
+      num_workers   = 2
       spark_version = "11.0.x-cpu-ml-scala2.12"
       node_type_id  = "n1-standard-4"
       custom_tags   = { "clusterSource" = "mlops-stack-0-0" }
